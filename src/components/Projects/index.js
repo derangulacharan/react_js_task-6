31c9097ae0e063dcfb4a0c6c195b594cc9766722
derangulacharan <derangulacharan64@gmail.com>
2023-10-14 13:40:37 +0530

    first commit

---
+++ src/components/Projects/index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCards'
+import { projects } from '../../data/constants'
+
+
+const Projects = ({openModal,setOpenModal}) => {
+  const [toggle, setToggle] = useState('all');
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+          I have worked on a wide range of projects. From research papers to android apps. Here are some of my projects.
+        </Desc>
+        <ToggleButtonGroup >
+          {toggle === 'all' ?
+            <ToggleButton active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+            :
+            <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'Research paper' ?
+            <ToggleButton active value="research paper" onClick={() => setToggle('research paper')}>research paper'S</ToggleButton>
+            :
+            <ToggleButton value="research paper" onClick={() => setToggle('research paper')}>research paper'S</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'projects' ?
+            <ToggleButton active value="projects" onClick={() => setToggle('projects')}>projects'S</ToggleButton>
+            :
+            <ToggleButton value="projects" onClick={() => setToggle('projects')}>ANDROID APP'S</ToggleButton>
+          }
+          <Divider />
+          {/* {toggle === 'machine learning' ?
+            <ToggleButton active value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
+            :
+            <ToggleButton value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
+          } */}
+        </ToggleButtonGroup>
+        <CardContainer>
+          {toggle === 'all' && projects
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+          {projects
+            .filter((item) => item.category == toggle)
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
